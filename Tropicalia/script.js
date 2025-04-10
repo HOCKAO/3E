03c0645b49ed142b4fbe33842962e732d1d9d184
Luan Machado <142520449+HOCKAO@users.noreply.github.com>
2025-04-10 20:58:11 -0300

    Add files via upload

---
+++ Tropicalia/script.js
@@ -0,0 +1,42 @@
+document.addEventListener('DOMContentLoaded',function)
+
+    const botaoDeAcessibilidade=document.getElementById('botao-acessibilidade');
+    const opcoesDeAcessbilidade=document.getElementById('opcoes-acessibilidade');
+    
+    botaoDeAcessibilidade.addEventListener('click',function(){
+        botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+        opcoesDeAcessbilidade.classList.toggle('apresenta-lista');
+    
+    const botaoSelecionado=botaoDeAcessibilidade.getAttribute('aria-expanded')=== 'true';
+
+    botaoDeAcessibilidade.setAttribute('aria-expanded', !botaoSelecionado)
+
+    })
+
+    const aumentaFonBotao = document.getElementById('aumentar-fonte');
+    const diminuirFonteBOtao = document.getElementById('diminuir-fonte');
+
+    const alternaContraste = document.getElementById('alterna-contraste')
+
+    let tamanhoAtualFonte = 1;
+
+    aumentaFonteBotao.addEventListener('click', function(){
+         tamanhoAtualFonte += 0.1;
+         document.body.style.fontSize = '${tamanhoAtualFonte}rem'
+    })
+
+    diminuiFonteBotao.addEventListener('click', function() {
+        tamanhoAtualFonte -=0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem´
+    })
+
+    alternaContraste.addEventListener('click', function(){
+        document.body.classList.toggle('alto-contraste')
+    })
+
+})
+
+ScrollReveal().reveal('#inicio', { delay: 500 });
+ScrollReveal().reveal('#tropicalia', { delay: 500});
+ScrollReveal().reveal('#galeria', { delay: 500});
+ScrollReveal().reveal('#contato', { delay: 500});
